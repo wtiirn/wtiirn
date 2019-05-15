a616436fe989c596bab513b0da08709d14182d0a
Dorian Scheidt <dorian.scheidt@gmail.com>
2019-05-14 19:07:03 -0700

    Extract page content to functions

--- src/main.rs
+++ src/main.rs
@@ -9,16 +9,22 @@ fn main() {
         println!("Request received. {} {}", request.method(), request.uri());
 
         match (request.method(), request.uri().path()) {
-            (&Method::GET, "/hello") => {
-                Ok(response.body("<html><h1>Hi!</h1><p>Hello Rust!</p></html>".as_bytes().to_vec())?)
-            }
+            (&Method::GET, "/") => Ok(response.body(home_page().as_bytes().to_vec())?),
             (_, _) => {
                 response.status(StatusCode::NOT_FOUND);
-                Ok(response.body("<html><h1>404</h1><p>Not found!<p></html>".as_bytes().to_vec())?)
+                Ok(response.body(not_found_page().as_bytes().to_vec())?)
             }
         }
     });
 
     println!("Server listening on port: {}", port);
     server.listen(host, port);
+}
+
+fn home_page() -> String {
+    "<html><h1>Hi!</h1><p>Hello Rust!</p></html>".to_string()
+}
+
+fn not_found_page() -> String {
+    "<html><h1>404</h1><p>Not found!<p></html>".to_string()
 }
