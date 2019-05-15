f517a2ce5155b60e39b9ca6312ebddc3c368babb
Nat Knight <nathaniel.ep@gmail.com>
2019-05-14 20:38:27 -0700

    Types and serialization for scraped predictions

--- scraping/scrape_tide.py
+++ scraping/scrape_tide.py
@@ -1,4 +1,5 @@
 import datetime
+import json
 import pathlib
 import typing as ty
 
@@ -28,12 +29,12 @@ MONTHS = [
 TZINFO = datetime.timezone(offset=datetime.timedelta(hours=-8), name="PST")
 
 
-def parse_row(rw, month: str, year: int) -> TidePrediction:
+def parse_row(rw, month_src: str, year: int) -> TidePrediction:
     day_src, time_src, tide_src = [c.get_text() for c in rw.find_all("td")]
     tide = float(tide_src)
     time = datetime.datetime.strptime(time_src, "%I:%M %p").time()
     day = int(day_src)
-    month = MONTHS.index(month) + 1
+    month = MONTHS.index(month_src) + 1
     date = datetime.datetime(
         year=year,
         month=month,
@@ -45,7 +46,7 @@ def parse_row(rw, month: str, year: int) -> TidePrediction:
     return TidePrediction(date=date, tide=tide)
 
 
-def parse_table(tbl) -> ty.Tuple[str, ty.Iterable[TidePrediction]]:
+def parse_table(tbl) -> ty.Iterable[TidePrediction]:
     caption = tbl.caption.get_text()
     month, year = caption.split(" ")
     assert int(year) == 2019
@@ -56,14 +57,21 @@ def parse_table(tbl) -> ty.Tuple[str, ty.Iterable[TidePrediction]]:
     return [parse_row(rw, month, int(year)) for rw in rows]
 
 
-def parse_tides(src: str):
+def parse_tides(src: str) -> ty.Iterable[TidePrediction]:
     parsed = bs4.BeautifulSoup(src, "html.parser")
     tables = parsed.find_all("table")
-    yield from map(parse_table, tables)
+    for month in map(parse_table, tables):
+        yield from month
+
+
+def format_prediction(p: TidePrediction) -> str:
+    return json.dumps({"tide": p.tide, "time": p.date.isoformat()})
 
 
 if __name__ == "__main__":
-    import pprint
-
     src = pathlib.Path("point_atkinson_2019.html").open().read()
-    pprint.pprint(list(parse_tides(src)))
+    predictions = list(parse_tides(src))
+    with pathlib.Path("predictions.json").open("w") as outf:
+        outf.write("[\n")
+        outf.write(",\n".join([format_prediction(p) for p in predictions]))
+        outf.write("]")
