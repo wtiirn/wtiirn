e5065e1f18371f8d31072fc78918d2c43ab85a60
Dorian Scheidt <dorian.scheidt@gmail.com>
2019-05-17 00:26:14 -0700

    Refactor to use itertools

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,8 @@ use std::env;
 use chrono::prelude::*;
 use simple_server::{Method, Server, StatusCode};
 
-mod model;
 mod compute;
+mod model;
 
 static PREDICTIONS_SRC: &'static str = include_str!("predictions.json");
 
