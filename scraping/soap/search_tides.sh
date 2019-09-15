a5567ad1abff4d7d08ea31282350008c811d7210
Nat Knight <nathaniel.ep@gmail.com>
2019-09-15 15:27:41 -0700

    Remove deprecated scraping data and associated binary

--- scraping/soap/search_tides.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
- curl --header "Content-Type: text/xml;charset=UTF-8" https://ws-shc.qc.dfo-mpo.gc.ca/predictions --header "SOAPAction:search" --data @tides_gc_ca_predictions_search.xml | tidy -xml -i
-
