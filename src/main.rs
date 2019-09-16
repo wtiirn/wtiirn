2ccb7a4963ee56e2403b89f7c9cc2d9b8d113fa3
Dorian Scheidt <dorian.scheidt@gmail.com>
2019-09-15 17:07:40 -0700

    First try at http -> https redirection
    This change uses the `x-forwarded-proto` header to check if heroku has
    transmited the request from http, and returns a 301 redirect to the
    https url in that case.
    
    In order to test this, I extracted the route handler to a function,
    which means we can now unit test our routes very easily.
    
    I've been able to manually test that the 301 is returned locally, but
    I'm still not 100% confident this will work how we expect. Fingers
    crossed!

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,8 @@ use std::convert::TryInto;
 use std::env;
 use wtiirn::{pages, stations};
 
-use simple_server::{Method, Server, StatusCode};
+use http::header::{self, HeaderName};
+use simple_server::{Handler, Method, Request, ResponseBuilder, Server, StatusCode};
 
 fn main() {
     let host = env::var("WTIIRN_HOST").unwrap_or_else(|_| "127.0.0.1".to_string());
@@ -11,23 +12,75 @@ fn main() {
     let catalogue = stations::StationCatalogue::load();
 
     println!("WTIIRN booting up!");
-    let server = Server::new(move |request, mut response| {
-        println!("Request received. {} {}", request.method(), request.uri());
-        let coords = request.uri().query().try_into().ok();
-
-        match (request.method(), request.uri().path()) {
-            (&Method::GET, "/") => Ok(response.body(
-                pages::home_page(pages::HomePageViewModel::new(&catalogue, &coords))
-                    .as_bytes()
-                    .to_vec(),
-            )?),
-            (_, _) => {
-                response.status(StatusCode::NOT_FOUND);
-                Ok(response.body(pages::not_found_page().as_bytes().to_vec())?)
-            }
-        }
-    });
+    let server = Server::new(routes(catalogue));
 
     println!("Server listening on port: {}", port);
     server.listen(&host, &port);
+}
+
+fn routes(catalogue: stations::StationCatalogue) -> Handler {
+    let forwarded_proto_header = HeaderName::from_static("x-forwarded-proto");
+
+    Box::new(
+        move |request: Request<Vec<u8>>, mut response: ResponseBuilder| {
+            println!("Request received. {} {}", request.method(), request.uri());
+            let coords = request.uri().query().try_into().ok();
+
+            match (
+                request.method(),
+                request.uri().path(),
+                request
+                    .headers()
+                    .get(&forwarded_proto_header)
+                    .map(|x| x.len()),
+            ) {
+                (_, _, Some(4)) => {
+                    response
+                        .status(StatusCode::MOVED_PERMANENTLY)
+                        .header(header::LOCATION, "https://whattideisitrightnow.com");
+                    Ok(response.body(vec![])?)
+                }
+                (&Method::GET, "/", _) => Ok(response.body(
+                    pages::home_page(pages::HomePageViewModel::new(&catalogue, &coords))
+                        .as_bytes()
+                        .to_vec(),
+                )?),
+                (_, _, _) => {
+                    response.status(StatusCode::NOT_FOUND);
+                    Ok(response.body(pages::not_found_page().as_bytes().to_vec())?)
+                }
+            }
+        },
+    )
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use simple_server::Response;
+    #[test]
+    fn it_should_redirect_in_http() {
+        let routes = routes(stations::StationCatalogue::test());
+        let request = Request::builder()
+            .header("x-forwarded-proto", "http")
+            .body(vec![])
+            .unwrap();
+
+        let response = routes(request, Response::builder()).unwrap();
+
+        assert_eq!(response.status(), StatusCode::MOVED_PERMANENTLY);
+    }
+
+    #[test]
+    fn it_should_not_redirect_in_https() {
+        let routes = routes(stations::StationCatalogue::test());
+        let request = Request::builder()
+            .header("x-forwarded-proto", "https")
+            .body(vec![])
+            .unwrap();
+
+        let response = routes(request, Response::builder()).unwrap();
+
+        assert_eq!(response.status(), StatusCode::OK);
+    }
 }
