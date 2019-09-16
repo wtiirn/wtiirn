368e7eae22a89c30191ce2070dd335acdb5c6fff
Dorian Scheidt <dorian.scheidt@gmail.com>
2019-09-15 17:58:51 -0700

    Update js to pass in timezone offset

--- public/getlocation.js
+++ public/getlocation.js
@@ -1,16 +1,28 @@
-function reloadWithLocation(pos) {
+function reloadWithLocationAndOffset(pos) {
   let coords = pos.coords;
   let latlon =
     "lat=" + coords.latitude.toString() + "&lon=" + coords.longitude.toString();
-  let newUrl = "/?" + latlon;
+  let newUrl = "/?" + latlon + "&offset=" + getLocalTimezoneOffset();
   window.location.replace(newUrl);
+}
+
+function reloadWithOffset() {
+  let offset = getLocalTimezoneOffset();
+  window.location.replace("/?offset=" + offset);
 }
 
 function getLocationAndReload() {
   if ("geolocation" in navigator) {
     let geo = navigator.geolocation;
-    geo.getCurrentPosition(reloadWithLocation, console.error);
+    geo.getCurrentPosition(reloadWithLocationAndOffset, reloadWithOffset);
+  } else {
+    reloadWithOffset();
   }
+}
+
+function getLocalTimezoneOffset() {
+  let date = new Date();
+  return date.getTimezoneOffset();
 }
 
 window.onload = function() {
