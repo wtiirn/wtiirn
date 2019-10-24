9b636efdd9a0c6728b8eb5e0d62b9a26d1ed06d1
Dorian Scheidt <dorian.scheidt@gmail.com>
2019-10-23 19:16:43 -0700

    Show a map centered on the tide station

--- public/getlocation.js
+++ public/getlocation.js
@@ -30,3 +30,13 @@ window.onload = function() {
     getLocationAndReload();
   }
 };
+
+function showMap(lat, lon) {
+  var map = L.map('map', {
+      center: [lat, lon],
+      zoom: 13
+  });
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: "&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors"
+  }).addTo(map);
+}
--- src/pages.rs
+++ src/pages.rs
@@ -120,14 +120,30 @@ impl HomePageViewModel {
             format!("Can't calculate current tide level")
         }
     }
+
+    fn station_lat(&self) -> f64 {
+        self.station.coordinates.lat
+    }
+
+    fn station_lon(&self) -> f64 {
+        self.station.coordinates.lon
+    }
 }
 
 pub fn home_page(vm: HomePageViewModel) -> String {
     format!(
-        "<html>
+        r#"<html>
             <head>
                 <title>What Tide Is It Right Now?!</title>
                 <link REL=stylesheet href='style.css' />
+
+                <link rel='stylesheet' href='https://unpkg.com/leaflet@1.5.1/dist/leaflet.css'
+                  integrity='sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=='
+                crossorigin=''/>
+                <script src='https://unpkg.com/leaflet@1.5.1/dist/leaflet.js'
+              integrity='sha512-GffPMF3RvMeYyc1LWMHtK8EbPv0iNZ8/oTtHPx9/cc2ILxQ+u905qIwdpULaqDkyBKgOaB57QTMg7ztg8Jm2Og=='
+                crossorigin=''></script>
+
             </head>
             <body>
                 <div class='container'>
@@ -148,16 +164,22 @@ pub fn home_page(vm: HomePageViewModel) -> String {
                             {}
                             <p>{}</p>
                         </div>
+                        <div id='map'></div>
                     </div>
                 </div>
                 <script src='getlocation.js'></script>
+                <script>
+                  showMap({}, {})
+                </script>
             </body>
-        </html>",
+        </html>"#,
         vm.current_time.format(TIME_FORMAT),
         vm.headline(),
         vm.current_level(),
         vm.detail(),
         vm.station_info(),
+        vm.station_lat(),
+        vm.station_lon(),
     )
 }
 
